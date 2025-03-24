702502ef195ed2594ec3d38b3014b47f448fc047
HP <chandan.workholics@gmail.com>
2025-03-24 16:17:53 +0530

    updated

--- admin/src/Pages/MonthlyAttendance.jsx
+++ admin/src/Pages/MonthlyAttendance.jsx
@@ -4,27 +4,53 @@ import Header from '../Components/Header'
 import ProfileImg from '../assets/images/pro-img.png'
 
 const MonthlyAttendance = () => {
-    const [employees, setEmployees] = useState([]);
     const [currentDate, setCurrentDate] = useState(new Date().toISOString().split("T")[0]);
+    const [attendanceData, setAttendanceData] = useState([]);
+    const [currentMonth, setCurrentMonth] = useState(new Date().getMonth() + 1);
+    const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
 
-    // Get Employees
-    const getEmployees = async () => {
-        try {
-            const response = await fetch("http://localhost:4000/api/users", {
-                method: "GET",
-                headers: { "Content-Type": "application/json" },
-            });
-            if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`)
-            const result = await response.json();
-            setEmployees(result);
-        } catch (error) {
-            console.error("Error fetching employees:", error.message);
+    const getDaysInMonth = (month, year) => {
+        return new Date(year, month, 0).getDate();
+    };
+
+    const daysInMonth = getDaysInMonth(currentMonth, currentYear);
+
+    // Fetch attendance data for the selected month
+    const fetchAttendanceData = () => {
+        const requestOptions = {
+            method: "GET",
+            redirect: "follow",
+        };
+
+        fetch(`http://localhost:4000/api/attendance?month=${currentMonth}&year=${currentYear}`, requestOptions)
+            .then((response) => response.json())
+            .then((data) => setAttendanceData(data))
+            .catch((error) => console.error("Error fetching attendance data:", error));
+    };
+
+    // Fetch data whenever the month or year changes
+    useEffect(() => {
+        fetchAttendanceData();
+    }, [currentMonth, currentYear]);
+
+    // Handle navigation between months
+    const handlePreviousMonth = () => {
+        if (currentMonth === 1) {
+            setCurrentMonth(12);
+            setCurrentYear((prevYear) => prevYear - 1);
+        } else {
+            setCurrentMonth((prevMonth) => prevMonth - 1);
         }
     };
 
-    useEffect(() => {
-        getEmployees();
-    }, []);
+    const handleNextMonth = () => {
+        if (currentMonth === 12) {
+            setCurrentMonth(1);
+            setCurrentYear((prevYear) => prevYear + 1);
+        } else {
+            setCurrentMonth((prevMonth) => prevMonth + 1);
+        }
+    };
 
     return (
         <>
@@ -37,10 +63,21 @@ const MonthlyAttendance = () => {
                         </div>
                         <div className="col-md-12 mb-4">
                             <div className="card bg-ffffff94 border-0 rounded-5 h-100">
-                                <div className="card-header pt-3 d-flex justify-content-between bg-transparent border-bottom-0">
-                                    <h4 className="mb-0 fw-bold ">Monthly Attendance</h4>
-                                    <div className="">
+                                <div className="card-header pt-3 d-block d-lg-flex justify-content-between bg-transparent border-bottom-0">
+                                    <h4 className="mb-3 mb-lg-0 fw-bold ">Monthly Attendance</h4>
+                                    <div className="mb-3 mb-lg-0 d-flex d-lg-block justify-content-center">
                                         <input type="date" value={currentDate} onChange={(e) => setCurrentDate(e.target.value)} />
+                                    </div>
+                                    <div className="d-flex justify-content-between align-items-center">
+                                        <button className="btn btn-warning rounded-pill" onClick={handlePreviousMonth}>
+                                            <i class="fa-solid fa-arrow-left"></i>
+                                        </button>
+                                        <h4 className='mx-2 mb-0'>
+                                            {new Date(currentYear, currentMonth - 1).toLocaleString("default", { month: "long" })} {currentYear}
+                                        </h4>
+                                        <button className="btn btn-warning rounded-pill" onClick={handleNextMonth}>
+                                            <i class="fa-solid fa-arrow-right"></i>
+                                        </button>
                                     </div>
                                 </div>
                                 <div className="card-body">
@@ -50,45 +87,35 @@ const MonthlyAttendance = () => {
                                                 <tr className="table-warning">
                                                     <th scope="col">#</th>
                                                     <th scope="col">Employee</th>
-                                                    <th scope="col">User Name</th>
-                                                    <th scope="col">Job Title</th>
-                                                    <th scope="col">Attendance</th>
-                                                    <th scope="col">Action</th>
+                                                    {Array.from({ length: daysInMonth }, (_, i) => (
+                                                        <th key={i + 1}>{i + 1}</th>
+                                                    ))}
                                                 </tr>
                                             </thead>
                                             <tbody>
-                                                {employees.length > 0 ? (
-                                                    employees.map((employee, index) => (
-                                                        <tr key={employee.id}>
-                                                            <th scope="row">{index + 1}</th>
-                                                            <td>
-                                                                <img src={ProfileImg} alt="" className="tbl-empImg" />
-                                                                {employee.name}
-                                                            </td>
-                                                            <td>{employee.username}</td>
-                                                            <td>{employee.role}</td>
-                                                            <td>
-                                                                <select class="form-select" aria-label="Default select example">
-                                                                    <option value="1" selected>Present</option>
-                                                                    <option value="2">Absent</option>
-                                                                    <option value="3">WFH</option>
-                                                                </select>
-                                                            </td>
-                                                            <td>
-                                                                <button type="button" className="btn btn-warning text-white rounded-5 me-3">
-                                                                    Edit
-                                                                    <span className="ms-2"><i className="fa-solid fa-user-pen"></i></span>
-                                                                </button>
-                                                            </td>
-                                                        </tr>
-                                                    ))
-                                                ) : (
-                                                    <tr>
-                                                        <td colSpan="9" className="text-center">
-                                                            No employees found
+                                                {attendanceData.map((employee, index) => (
+                                                    <tr key={index}>
+                                                        <td>{index + 1}</td>
+                                                        <td>
+                                                            <img src={ProfileImg} alt="" className="tbl-empImg" />
+                                                            {employee.name}
                                                         </td>
+                                                        {Array.from({ length: daysInMonth }, (_, i) => (
+                                                            <td
+                                                                key={i}
+                                                                className={
+                                                                    employee.days && employee.days[i] === "✔️"
+                                                                        ? "text-success"
+                                                                        : employee.days && employee.days[i] === "🔶"
+                                                                            ? "text-warning"
+                                                                            : "text-danger"
+                                                                }
+                                                            >
+                                                                {employee.days && employee.days[i] ? employee.days[i] : ""}
+                                                            </td>
+                                                        ))}
                                                     </tr>
-                                                )}
+                                                ))}
                                             </tbody>
                                         </table>
                                     </div>

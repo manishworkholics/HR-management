8db6fd18ac4d0aca95c762e97ddfbe1aab68355e
HP <chandan.workholics@gmail.com>
2025-03-24 14:59:00 +0530

    updated

--- admin/src/index.js
+++ admin/src/index.js
@@ -10,29 +10,19 @@ import Employee from './Pages/Employee';
 import Attendance from './Pages/Attendance';
 import LeaveRequest from './Pages/LeaveRequest';
 import Login from './Pages/Login';
-<<<<<<< HEAD
 import ProtectedRoute from './Pages/Common_Method/protectedroute'
-=======
 import MonthlyAttendance from './Pages/MonthlyAttendance';
->>>>>>> d97e353b5d2c1a7a7faa82a3444a868d17bd3b71
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path='/login' element={<Login />} />
-<<<<<<< HEAD
-      <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />}  />} />
-      <Route path='/employee' element={<ProtectedRoute element = {<Employee />}/>} />
-      <Route path='/attendance' element={<ProtectedRoute element = {<Attendance/>} />} />
-      <Route path='/leave-request' element={<ProtectedRoute element = {<LeaveRequest/>}/>} />
-=======
-      <Route path='/dashboard' element={<Dashboard />} />
-      <Route path='/employee' element={<Employee />} />
-      <Route path='/attendance' element={<Attendance />} />
-      <Route path='/monthly-attendance' element={<MonthlyAttendance />} />
-      <Route path='/leave-request' element={<LeaveRequest />} />
->>>>>>> d97e353b5d2c1a7a7faa82a3444a868d17bd3b71
+      <Route path='./login' element={<Login />} />
+      <Route path="./dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
+      <Route path='./employee' element={<ProtectedRoute element={<Employee />} />} />
+      <Route path='./attendance' element={<ProtectedRoute element={<Attendance />} />} />
+      <Route path='./leave-request' element={<ProtectedRoute element={<LeaveRequest />} />} />
+      <Route path='./monthly-attendance' element={<MonthlyAttendance />} />
     </Routes>
   </BrowserRouter>
 );
